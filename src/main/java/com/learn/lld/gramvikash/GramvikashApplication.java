cef7b252101b8953eb6c7da0ffc65282a10a3490
bkk07 <180952443+bkk07@users.noreply.github.com>
2026-02-24 20:20:25 +0530

    Code

---
+++ src/main/java/com/learn/lld/gramvikash/GramvikashApplication.java
@@ -0,0 +1,13 @@
+package com.learn.lld.gramvikash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GramvikashApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GramvikashApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/learn/lld/gramvikash/GramvikashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.lld.gramvikash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GramvikashApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
